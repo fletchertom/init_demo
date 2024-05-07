c8d653b6572e37d6cb2d2aa7b241335328c59eda
Tom Fletcher <tom.fletcher@contino.io>
2024-05-07 15:21:17 +0100

    updated source of module code

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "./modules/networking"
+  source = "git@github.com:fletchertom/terraform-networking.git?ref=1.0.0"
 
   rgs                     = var.rgs
   vnets                   = var.vnets
