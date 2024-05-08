431bb6406f73f4ca28e5b9e4bba2daf8729fe966
Tom Fletcher <tom.fletcher@contino.io>
2024-05-08 10:37:55 +0100

    update to 1.1.0

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "git@github.com:fletchertom/terraform-networking.git?ref=1.0.0"
+  source = "git@github.com:fletchertom/terraform-networking.git?ref=1.1.0"
 
   rgs                     = var.rgs
   vnets                   = var.vnets
